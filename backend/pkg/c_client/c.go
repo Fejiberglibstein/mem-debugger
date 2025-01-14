193af46e631d4446a253fc8062afce401d80fc22
Fejiberglibstein <amthein321@gmail.com>
2025-01-14 18:34:36 -0500

    TCP connection for debugger

--- backend/pkg/c_client/c.go
+++ backend/pkg/c_client/c.go
@@ -2,13 +2,19 @@ package c_client
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"log"
+	"net"
 	"os/exec"
 	"reflect"
+	"time"
 
 	debugger "github.com/Fejiberglibstein/mem-debugger/debugger/pkg"
 	"github.com/google/go-dap"
 )
+
+const PORT = "8921"
 
 type CClient struct {
 	debugger.Debugger
@@ -16,16 +22,38 @@ type CClient struct {
 }
 
 func (c *CClient) Start() error {
-	c.cmd = exec.Command("../installed_debuggers/codelldb/extension/adapter/codelldb")
-
-	c.Debugger = debugger.NewDebugger(
-		bufio.NewWriter(c.cmd.Stdout),
-		bufio.NewReader(c.cmd.Stdin),
+	c.cmd = exec.Command(
+		"../installed_debuggers/codelldb/extension/adapter/codelldb",
+		"--port",
+		PORT,
 	)
 
 	if err := c.cmd.Start(); err != nil {
 		return err
 	}
+
+	var conn net.Conn
+	var err error
+	tries := 0
+	for {
+		time.Sleep(500 * time.Millisecond)
+		conn, err = net.Dial("tcp", net.JoinHostPort("localhost", PORT))
+
+		if err == nil {
+			break
+		}
+
+		log.Print(err)
+		tries += 1
+		if tries > 10 {
+			return errors.New("Could not connect to the debugger for codelldb on port " + PORT)
+		}
+	}
+
+	c.Debugger = debugger.NewDebugger(
+		bufio.NewWriter(conn),
+		bufio.NewReader(conn),
+	)
 
 	res, err := c.SendAndWait(&dap.InitializeRequest{
 		Arguments: dap.InitializeRequestArguments{
@@ -48,7 +76,9 @@ func (c *CClient) Start() error {
 			SupportsInvalidatedEvent:            false,
 			SupportsStartDebuggingRequest:       false,
 		},
-	}, reflect.TypeOf(dap.InitializeResponse{}))
+	},
+		reflect.TypeOf(dap.InitializeResponse{}),
+	)
 	if err != nil {
 		return err
 	}
